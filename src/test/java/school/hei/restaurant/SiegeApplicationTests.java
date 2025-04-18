574b4b973f0455d432478c077a30a51f15c68e4c
AroNicolas <hei.nicolas.5@gmail.com>
2025-04-18 10:02:07 +0300

    init: initialize restaurant management project for siege side

---
+++ src/test/java/school/hei/restaurant/SiegeApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
